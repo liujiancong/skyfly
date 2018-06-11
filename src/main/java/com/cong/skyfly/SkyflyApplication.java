800a9064dc4d3de4afad4819eca2559f9685546c
liujiancong <1065308850@qq.com>
2018-06-11 11:03:50 +0800

    import preject firstly

---
+++ src/main/java/com/cong/skyfly/SkyflyApplication.java
@@ -0,0 +1,12 @@
+package com.cong.skyfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyflyApplication.class, args);
+	}
+}
