2779c8990964e3cff19bdd746aab34db18fe259d
liujiancong <1065308850@qq.com>
2018-06-11 11:24:10 +0800

    add a index controller

---
+++ src/main/java/com/cong/skyfly/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.cong.skyfly.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liujiancong on 2018/6/11.
+ */
+@RestController
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(){
+
+        return "index";
+    }
+}
